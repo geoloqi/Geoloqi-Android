e9c0a313bbdc2dd0913ed8c63be23b2ec9c99e3f
Aaron Parecki <aaron@parecki.com>
2011-01-22 13:12:14 -0800

    Adds location listener and logs location to the console

--- src/com/geoloqi/android1/GeoloqiService.java
+++ src/com/geoloqi/android1/GeoloqiService.java
@@ -2,14 +2,20 @@ package com.geoloqi.android1;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
-public class GeoloqiService extends Service {
+public class GeoloqiService extends Service implements LocationListener {
 	private static final String TAG = "GeoloqiService";
 	MediaPlayer player;
+	LocationManager locationManager;
 	
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -23,6 +29,8 @@ public class GeoloqiService extends Service {
 		
 		player = MediaPlayer.create(this, R.raw.braincandy);
 		player.setLooping(false); // Set looping
+		
+		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 	}
 
 	@Override
@@ -37,5 +45,30 @@ public class GeoloqiService extends Service {
 		Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
 		Log.d(TAG, "onStart");
 		player.start();
+		
+		String bestProvider = locationManager.getBestProvider(new Criteria(), true);
+		locationManager.requestLocationUpdates(bestProvider, 5, 10.0f, this);
+		
+		Log.d(TAG, "Provider: " + bestProvider);
+	}
+
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		Log.d(TAG, location.toString());
+	}
+
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
 	}
 }
