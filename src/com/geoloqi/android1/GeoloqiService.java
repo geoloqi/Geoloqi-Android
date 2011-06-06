1b19e714ab318dc5e3e45acc13d02476591b48db
Brian Ledger <bpl29@cornell.edu>
2011-06-06 13:23:21 -0700

    My first update.

--- src/com/geoloqi/android1/GeoloqiService.java
+++ src/com/geoloqi/android1/GeoloqiService.java
@@ -13,7 +13,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
